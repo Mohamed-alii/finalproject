e53507de2cea2836e00c97fb2d59fafa0c9edc51
Ghada1996 <dodymagdy908@gmail.com>
2021-04-19 16:37:21 -0700

    ghada commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import{ SharedModule} from './shared/shared.module';
 import{MainModule} from './main/main.module';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +14,6 @@ import {FormsModule} from '@angular/forms'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     SharedModule,
     MainModule,
     FormsModule
--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -3,7 +3,8 @@ import {FormGroup,Validators,FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  
 })
 export class HomeComponent implements OnInit {
 
